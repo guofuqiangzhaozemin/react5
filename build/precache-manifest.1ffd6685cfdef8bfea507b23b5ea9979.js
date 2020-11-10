4fd84383ae625e3ab35d900a94d312bc99d2099d
赵泽敏 <1963765653@qq.com>
2020-11-10 20:17:53 +0800

    react5

---
+++ build/precache-manifest.1ffd6685cfdef8bfea507b23b5ea9979.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da5491d5573ab7a4b59c57c279d1e43f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47446357fe5ed7f9c9df",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "47446357fe5ed7f9c9df",
+    "url": "/static/js/2.dd8e4d2c.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/static/js/2.dd8e4d2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "17a2c9ee9e2ef9163d0f",
+    "url": "/static/js/main.ca2ff8e3.chunk.js"
+  },
+  {
+    "revision": "e2702865a21868f2ebe6",
+    "url": "/static/js/runtime-main.7918a0be.js"
+  }
+]);
